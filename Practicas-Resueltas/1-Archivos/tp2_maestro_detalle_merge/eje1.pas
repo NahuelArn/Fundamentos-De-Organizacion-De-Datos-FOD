5a2b8a5a5a3c81153e1e30138b1f2cf4cdab4039
Nahuel <nahuelarancibia4@gmail.com>
2024-03-21 22:01:17 -0300

    fx

--- Practicas-Resueltas/1-Archivos/tp2_maestro_detalle_merge/eje1.pas
+++ Practicas-Resueltas/1-Archivos/tp2_maestro_detalle_merge/eje1.pas
@@ -14,14 +14,14 @@ begin
   Writeln('ESTOY TRAYENDO BIEN???');
   Assign(archivo_provisto, 'D:\Codigo\Fundamentos-De-Organizacion-De-Datos-FOD\Practicas-Resueltas\1-Archivos\tp2_maestro_detalle_merge\archivos_txt\archivo_provisto.txt');
   //no lo dejo abierto, cuando lo necesite lo abro
-  reset(archivo_provisto);
-  while (not eof (archivo_provisto)) do
-    begin
-      readln(archivo_provisto, empl.cod_empleado, empl.nombre,empl.monto_comision);
-      Writeln('Cod de empleado: ',empl.cod_empleado,' comision: ',empl.monto_comision, ' nombre empleado: ',empl.nombre, ' | ');
-    end;
-    Close(archivo_provisto);
-    Writeln('ESTOY TRAYENDO BIEN???');
+  // reset(archivo_provisto);
+  // while (not eof (archivo_provisto)) do
+  //   begin
+  //     readln(archivo_provisto, empl.cod_empleado,empl.monto_comision,empl.nombre);
+  //     Writeln('Cod de empleado: ',empl.cod_empleado,' comision: ',empl.monto_comision, ' nombre empleado: ',empl.nombre, ' | ');
+  //   end;
+  //   Close(archivo_provisto);
+  //   Writeln('ESTOY TRAYENDO BIEN???');
 end;
 
 procedure inicializar_nuevo_archivo(var archivo_compactado: archivo_fisico);
@@ -49,7 +49,7 @@ var
   acumulador: empleado; //mejor tenerlo como registro
 begin
   reset(archivo_provisto);  //arch consumbo abierto
-  readln(archivo_provisto, empl.cod_empleado, empl.nombre,empl.monto_comision);
+  readln(archivo_provisto, empl.cod_empleado, empl.monto_comision,empl.nombre);
   while((not eof(archivo_provisto)))do
     begin
       // readln(archivo_provisto, empl.cod_empleado, empl.nombre,empl.monto_comision);
@@ -59,7 +59,7 @@ begin
       while((not eof(archivo_provisto) and (cod_actual = empl.cod_empleado)))do
         begin
           acumulador.monto_comision:= empl.monto_comision+ acumulador.monto_comision;
-          readln(archivo_provisto, empl.cod_empleado, empl.nombre,empl.monto_comision);
+          readln(archivo_provisto, empl.cod_empleado, empl.monto_comision,empl.nombre);
           WriteLn('LLego aca0?');
         end;
         //sali, 2 motivos, finalizo el recorrido o cambio de codigo
@@ -84,7 +84,7 @@ begin
     begin
       // read(archivo_compactado, empl.cod_empleado, empl.nombre, empl.monto_comision); //solo los archivos ya cargados te lo traes campo x campo
       read(archivo_compactado, empl);
-      Write('Cod de empleado: ',empl.cod_empleado, ' nombre: empleado: ',empl.nombre,' comision: ',empl.monto_comision, ' | ');
+      Write('Cod de empleado: ',empl.cod_empleado, ' nombre: empleado: ',empl.nombre,' comision: ',empl.monto_comision:2:2, ' | ');
     end;
   close(archivo_compactado);
 end;
@@ -94,11 +94,11 @@ var
   archivo_compactado: archivo_fisico;
 begin
   traerme_el_archivo(archivo_provisto);
-  // inicializar_nuevo_archivo(archivo_compactado);
-  // {Q tengo en este punto? cree y abri el archivo compactado, el archivo de consumo lo tengo cerrado}
-  // compactar_archivo(archivo_provisto, archivo_compactado);
-  // //en este punto ya genere el nuevo archivo y lo tendria que tener cargado (dentro del modulo cerre los 2 archivos)
+  inicializar_nuevo_archivo(archivo_compactado);
+  {Q tengo en este punto? cree y abri el archivo compactado, el archivo de consumo lo tengo cerrado}
+  compactar_archivo(archivo_provisto, archivo_compactado);
+  //en este punto ya genere el nuevo archivo y lo tendria que tener cargado (dentro del modulo cerre los 2 archivos)
   
-  // {So good?}
-  // imprimir(archivo_compactado); 
+  {So good?}
+  imprimir(archivo_compactado); 
 end.
--- Practicas-Resueltas/1-Archivos/tp2_maestro_detalle_merge/tempCodeRunnerFile.pas
+++ Practicas-Resueltas/1-Archivos/tp2_maestro_detalle_merge/tempCodeRunnerFile.pas
@@ -1,1 +1,8 @@
-real
+  // reset(archivo_provisto);
+  // while (not eof (archivo_provisto)) do
+  //   begin
+  //     readln(archivo_provisto, empl.cod_empleado,empl.monto_comision,empl.nombre);
+  //     Writeln('Cod de empleado: ',empl.cod_empleado,' comision: ',empl.monto_comision, ' nombre empleado: ',empl.nombre, ' | ');
+  //   end;
+  //   Close(archivo_provisto);
+  //   Writeln('ESTOY TRAYENDO BIEN???');
